f60e3c8590627fa6726fc2c70aa68508473c4939
nialamide <146184063+nialamide@users.noreply.github.com>
2023-11-18 18:02:28 +0500

    Delete mod8_3 directory

--- mod8_3/src/main/java/com/example/hw8_2/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hw8_2.dao;
-
-import com.example.hw8_2.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
